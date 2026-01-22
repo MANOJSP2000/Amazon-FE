b4fec8c19388007a75c176f080cee60d3e12cc08
MANOJSP2000 <spmanoj2000@gmail.com>
2026-01-22 15:05:17 +0530

    Update other-packages.sh

--- other-packages.sh
+++ other-packages.sh
@@ -1,30 +1,63 @@
 #!/bin/bash
-sudo apt update
+set -e
 
-#install terraform
-sudo apt-get install unzip wget -y
-sudo wget https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_amd64.zip
-sudo unzip terraform_1.7.5_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
+echo "===== Installing Trivy, Terraform, kubectl, AWS CLI, Helm ====="
 
-#install Kubectl on Jenkins
-sudo apt update
-sudo apt install curl -y
-curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+# --------------------------------------------------
+# Update system & install common dependencies
+# --------------------------------------------------
+sudo apt update -y
+sudo apt install -y \
+  wget \
+  curl \
+  gnupg \
+  lsb-release \
+  unzip \
+  apt-transport-https
+
+# --------------------------------------------------
+# Install Trivy
+# --------------------------------------------------
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | \
+sudo gpg --dearmor -o /usr/share/keyrings/trivy.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] \
+https://aquasecurity.github.io/trivy-repo/deb \
+$(lsb_release -sc) main" | \
+sudo tee /etc/apt/sources.list.d/trivy.list
+
+sudo apt update -y
+sudo apt install -y trivy
+
+# --------------------------------------------------
+# Install Terraform
+# --------------------------------------------------
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com \
+$(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update -y
+sudo apt install -y terraform
+
+# --------------------------------------------------
+# Install kubectl
+# --------------------------------------------------
+curl -LO "https://dl.k8s.io/release/$(curl -L -s \
+https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
 kubectl version --client
 
-#install Aws cli
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt-get install unzip -y
+# --------------------------------------------------
+# Install AWS CLI v2
+# --------------------------------------------------
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" \
+-o awscliv2.zip
+
 unzip awscliv2.zip
-sudo ./aws/install
-
-#install helm.
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-
-# install trivy
-sudo su -
-curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.20.2
+sudo .
